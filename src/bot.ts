b93f1532f87e0e69238d0a51e033df961966832f
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2023-06-23 15:02:45 -0300

    changed msg

--- src/bot.ts
+++ src/bot.ts
@@ -4,12 +4,7 @@ import express from "express";
 // Create a bot using the Telegram token
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
+const introductionMessage = `is this thing working??`;
 
 const replyWithIntro = (ctx: any) =>
   ctx.reply(introductionMessage, {
