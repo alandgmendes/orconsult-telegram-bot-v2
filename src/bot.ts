96653a190a69965305b08689a5218aef9d20a0c2
Alandouglas Godinho Mendes <adgmendes@gmail.com>
2023-06-23 14:48:09 -0300

    Build the Telegram bot.

---
+++ src/bot.ts
@@ -0,0 +1,35 @@
+import { Bot, webhookCallback } from "grammy";
+import express from "express";
+
+// Create a bot using the Telegram token
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+
+const introductionMessage = `Hello! I'm a Telegram bot.
+I'm powered by Cyclic, the next-generation serverless computing platform.
+
+<b>Commands</b>
+/yo - Be greeted by me
+/effect [text] - Show a keyboard to apply text effects to [text]`;
+
+const replyWithIntro = (ctx: any) =>
+  ctx.reply(introductionMessage, {
+    parse_mode: "HTML",
+  });
+
+bot.on("message", replyWithIntro);
+
+// Start the server
+if (process.env.NODE_ENV === "production") {
+    // Use Webhooks for the production server
+    const app = express();
+    app.use(express.json());
+    app.use(webhookCallback(bot, "express"));
+  
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Bot listening on port ${PORT}`);
+    });
+  } else {
+    // Use Long Polling for development
+    bot.start();
+  }
